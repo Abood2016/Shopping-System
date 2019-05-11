90d38f8b652b7740467aab17b8802e08cffaab45
Abood2016 <abooody4693@gmail.com>
2019-05-11 07:05:26 +0300

    finished the users section

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
+use App\Order;
+use App\Product;
 class DashboardController extends Controller
 {
 
     public function index()
     {
-        return view('admin.dashboard');
+        $user = new User();
+        $order = new Order();
+        $product = new Product();
+        return view('admin.dashboard',compact('user','order','product'));
     }
 
     
---
+++ resources/views/admin/users/index.blade.php
@@ -0,0 +1,67 @@
+@extends('admin.layouts.master')
+
+@section('page')
+    Users
+@endsection
+
+@section('content')
+
+<div class="row">
+                    <div class="col-md-12">
+                @include('admin.layouts.messages')
+                        <div class="card">
+                            <div class="header">
+                                <h4 class="title">Users</h4>
+                                <p class="category">List of all registered users</p>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table class="table table-striped">
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Registered At</th>
+                                        <th>Status</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                    <tbody>
+                                 @foreach($users as $user)       
+                                    <tr>
+                                        <td>{{ $user->id }}</td>
+                                        <td>{{ $user->name }}</td>
+                                        <td>{{ $user->email }}</td>
+                                        <td>{{ $user->created_at->diffForhumans() }}</td>
+
+                                        <td>
+                                         @if($user->status)
+                                        <span class="label label-success">Active</label>
+
+                                         @else
+                                        <span class="label label-warning">Block</label>
+
+                                            @endif
+                                      </td>
+                                        <td>
+                                        @if($user->status)
+                                           <a href="{{ route('user.block',['id' => $user->id]) }}" class="btn btn-warning btn-sm"
+                                                    title="block">block</a>
+                                            @else
+                                            <a href="{{ route('user.active',['id' => $user->id]) }}" class="btn btn-success btn-sm"
+                                                    title="active">Active</a>
+                                           @endif 
+                                           <a href="{{ route('user.show',['id' => $user->id])}}" class="btn btn-success btn-sm"
+                                                    title="details">Details</a> 
+                                            
+                                        </td>
+                                        </tr>
+
+                                    @endforeach
+                                    </tbody>
+                                </table>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+@endsection
