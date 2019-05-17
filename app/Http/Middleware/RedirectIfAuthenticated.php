9f85bbb097615ca4e3ddba47807d631c46eb0c72
Abood2016 <abooody4693@gmail.com>
2019-05-17 19:18:43 +0300

    few final touch

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -15,22 +15,22 @@ class RedirectIfAuthenticated
      * @param  string|null  $guard
      * @return mixed
      */
+
     public function handle($request, Closure $next, $guard = null)
     {
+        //add guard
         switch ($guard) {
             case 'admin':
                 if (Auth::guard($guard)->check()) {
                     return redirect('/admin');
                 }
             break;
-
             default:
                 if (Auth::guard($guard)->check()) {
                     return redirect('/user/profile');
                 }
             break;
         }
-
         return $next($request);
     }
 }
