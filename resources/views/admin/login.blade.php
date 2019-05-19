79dc305d799cca9809d665cd3b76807c6115fdfd
Tqanoo20 <th2896228@gmail.com>
2019-05-19 12:32:42 +0300

    new login page and login attempt 2

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -74,14 +74,7 @@
           </div>
         </form>
       </div>
-      <div class="card-footer">
-        <div class="d-flex justify-content-center links">
-          Don't have an account?<a href="#">Sign Up</a>
-        </div>
-        <div class="d-flex justify-content-center">
-          <a href="#">Forgot your password?</a>
-        </div>
-      </div>
+      
     </div>
   </div>
 </div>
